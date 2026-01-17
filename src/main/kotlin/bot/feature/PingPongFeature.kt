ce7a85c494f5a4aaa2ca3dd67061ddf76aea8df1
c4vxl <manage@c4vxl.de>
2026-01-17 01:00:44 +0100

    Renamed PingPong -> PingPongFeature

--- src/main/kotlin/bot/feature/PingPongFeature.kt
+++ src/main/kotlin/bot/feature/PingPongFeature.kt
@@ -4,7 +4,7 @@ import de.c4vxl.bot.Bot
 import de.c4vxl.bot.feature.type.Feature
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
-class PingPong(bot: Bot) : Feature<PingPong>(bot, PingPong::class.java) {
+class PingPongFeature(bot: Bot) : Feature<PingPongFeature>(bot, PingPongFeature::class.java) {
     init {
         bot.commandHandler.registerSlashCommand(
             Commands.slash("ping", "Play ping-pong with the bot!")
