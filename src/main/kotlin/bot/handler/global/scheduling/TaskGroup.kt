9cf4ead7e53c031bc36f4c3aa992fd30c42497fd
c4vxl <manage@c4vxl.de>
2026-03-15 17:15:07 +0100

    Added TaskGroup.register

--- src/main/kotlin/bot/handler/global/scheduling/TaskGroup.kt
+++ src/main/kotlin/bot/handler/global/scheduling/TaskGroup.kt
@@ -17,13 +17,32 @@ class TaskGroup(val guild: Guild) {
     private val tasks = mutableListOf<ScheduledFuture<*>>()
 
     /**
+     * Registers a scheduled future to cache
+     * @param handler The function creating the future
+     */
+    fun register(handler: () -> ScheduledFuture<*>) {
+        tasks += handler.invoke()
+    }
+
+    /**
      * Schedules a task
      * @param delay The delay for when the task should run
      * @param task The task to run
      * @param unit The time unit the delay is in
      */
     fun schedule(delay: Long, task: () -> Unit, unit: TimeUnit = TimeUnit.SECONDS) {
-        tasks += Scheduler.executor.schedule(task, delay, unit)
+        register { Scheduler.executor.schedule(task, delay, unit) }
+    }
+
+    /**
+     * Schedules a task at a fixed rate
+     * @param initialDelay The initial delay to wait before running the task the first time
+     * @param period The waiting period between two runs
+     * @param task The task to run
+     * @param unit The time unit the delay is in
+     */
+    fun scheduleAtFixedRate(initialDelay: Long, period: Long, task: () -> Unit, unit: TimeUnit = TimeUnit.SECONDS) {
+        register { Scheduler.executor.scheduleAtFixedRate(task, initialDelay, period, unit) }
     }
 
     /**
